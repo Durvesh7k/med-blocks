21cfa4821f870f541c8245be923c3371f4fc85d6
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-03-02 19:37:00 +0530

    Details component updated

--- src/components/Details.js
+++ src/components/Details.js
@@ -13,10 +13,10 @@ const Details = () => {
     return (
         <>
 
-            <div className='pt-8 h-screen w-screen bg-gradient-to-r from-[#38065d] to-[#3e0765] flex justify-center items-center overflow-hidden'>
-                <div className='flex space-x-24 w-11/12 mx-40'>
-                <img src={record} alt="" className='h-[30rem] border-4 border-black' />
-                <div className='text-white'>
+            <div className='pt-36 w-screen bg-gradient-to-r from-[#38065d] to-[#3e0765] flex  justify-center items-center overflow-hidden pb-20 lg:pb-40'>
+                <div className='flex lg:space-x-24 w-11/12 flex-col lg:flex-row justify-center items-center lg:items-start'>
+                <img src={record} alt="" className=' h-72 w-72 sm:h-96 sm:w-96 md:h-80  lg:h-[30rem] lg:w-[30rem] border-4 border-black ' />
+                <div className='text-white mt-12 mb-20'>
                     <p className='tracking-wider font-bold text-4xl font-roboto break-all pb-4'>{data.heading}</p>
                     <hr />
                     <p className='text-justify mt-6'>{data.description}</p>
