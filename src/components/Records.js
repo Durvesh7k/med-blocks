009add39aebfe297c059a828de1c42fa4192db2d
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-03-05 15:13:30 +0530

    UI Updated...

--- src/components/Records.js
+++ src/components/Records.js
@@ -107,7 +107,7 @@ const NewRecords = () => {
     }
 
     return (
-        <div className='h-screen w-screen bg-gradient-to-tr from-slate-900 via-purple-900 to-slate-900 overflow-x-hidden  pb-40'>
+        <div className='w-screen bg-gradient-to-tr from-slate-900 via-purple-900 to-slate-900 overflow-x-hidden  pb-40'>
             {recordsArray.length !== null ? (
                 <div>
                     <div className="flex flex-col justify-center items-center">
@@ -139,7 +139,7 @@ const NewRecords = () => {
                             </div>
 
                         ) : (
-                            <Loading value='getting the records' />
+                            <Loading value='Getting the Records' />
                         )}
                     </div>
                     <div className='flex flex-col justify-center items-center'>
@@ -148,7 +148,7 @@ const NewRecords = () => {
                             <input onChange={e => setNewAddress(e.target.value)} type="text" className=' p-2 w-60 sm:w-96 rounded-lg bg-slate-600 outline-none text-white tracking-wider' placeholder='Enter the account address' />
                             <button onClick={setNewOwner} className='text-white bg-blue-600 sm:px-6 w-32 sm:w-40 py-[0.6rem] rounded-lg tracking-wider hover:scale-105 transition duration-200 font-semibold text-sm sm:text-[1rem] '>Give Access</button>
                         </div>
-                        <hr />
+                        <hr/>
                     </div>
 
                     <div className='flex justify-center items-center'>
